d1a162d40d501f646479a767c7dd1d6ab4d255ae
Dieguidev <diegogaraycullas@gmail.com>
2024-08-26 21:11:43 -0500

    ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./views/LoginPage";
 import RequerimentPage from "./components/RequerimentPage";
-import "react-toastify/dist/ReactToastify.css";
+import 'react-toastify/dist/ReactToastify.css';
 import { useState } from "react";
 
+
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const [authenticated, setAuthenticated] = useState(false)
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={<LoginPage setIsAuthenticated={setIsAuthenticated} />}
-        />
-        <Route
-          path="/requeriments" element={isAuthenticated ? <RequerimentPage /> : <Navigate to="/" replace />}
-        />
+        <Route path="/" element={<LoginPage setAuthenticated={setAuthenticated}/>} />
+        <Route path="/requeriments" element={<RequerimentPage authenticated={authenticated}/>} />
       </Routes>
     </BrowserRouter>
-    //<LoginPage/>
-    // <RequerimentPage/>
+
   );
 }
 
